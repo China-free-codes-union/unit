a05cc48d27c75a71de08a81b86787303346f7e6e
Simon <wz961681117@gmail.com>
2015-05-01 11:09:35 -0500

    Create package-info.java

---
+++ package-info.java
@@ -0,0 +1,12 @@
+/**
+ * 本报将陆续包含各种各样的单位
+ * 包括 时间，重量
+ * 
+ * 时间单位说明： 所有时间的单位，如小时，分钟，毫秒，秒等，都是以毫秒为对比对象而产生的int值，如1second = 1000；
+ */
+/**
+ * @author Simon Wang
+ * @version 32.876-2015
+ *
+ */
+package com.roselism.one.obj.unit;
