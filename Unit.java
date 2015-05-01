cc97803ffd71a05e6dcdd986dd3e24ca89663665
Simon <wz961681117@gmail.com>
2015-05-01 11:10:01 -0500

    Create Unit.java

---
+++ Unit.java
@@ -0,0 +1,10 @@
+package com.roselism.one.obj.unit;
+
+public abstract class Unit {
+    private int value;
+    public final static Time Time = new Time();
+
+    public int getValue() {
+	    return value;
+    }
+}
